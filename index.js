e5c215d9ca6d31c25fb6d4b95bb8908231263671
Stefan <stefan.armijo@gmail.com>
2018-09-09 20:27:24 -0700

    implement filter by object and test

--- index.js
+++ index.js
@@ -1,31 +1,54 @@
 const objectFilter = {};
 
 objectFilter.byArray = function byArray(object, filterArray) {
-  console.log(filterArray);
-  if (Object.keys(filterArray[0]).length > 1) {
-    return object.reduce((a, c) => {
-      filterArray.forEach((opt) => {
-        let toPush = true;
-
-        Object.keys(opt).forEach((key) => {
-          if (c[key] !== opt[key]) toPush = false;
-        });
-
-        if (toPush && !a.includes(c)) a.push(c);
-      });
-      return a;
-    }, []);
-  }
-
-  const key = Object.keys(filterArray[0])[0];
+  // if the objects in the filter array have multiple properties
+  // as in [{ id: 0, messageId:9 }]
   return object.reduce((a, c) => {
-    if (filterArray.some(e => e[key] === c[key])) a.push(c);
+    filterArray.forEach((opt) => {
+      let toPush = true;
+
+      Object.keys(opt).forEach((key) => {
+        if (c[key] !== opt[key]) toPush = false;
+      });
+
+      // this check (!a.includes) needs to be better. How do I deeply check for
+      // object equality here?
+      if (toPush && !a.includes(c)) a.push(c);
+    });
     return a;
   }, []);
 };
 
 objectFilter.byObject = function byObject(object, filterObject) {
+  const filters = filterObject;
+  let result = [];
 
+  // when filterObject has values that are arrays, { color: ['red', 'blue'] },
+  // (color is red or color is blue) add it to result for later filtering
+  Object.keys(filters).forEach((key) => {
+    if (Array.isArray(filters[key])) {
+      object.forEach((item) => {
+        if (filters[key].includes(item[key]) && !result.includes(item)) {
+          result.push(item);
+        }
+      });
+    }
+  });
+
+  Object.keys(filters).forEach((key) => {
+    if (Array.isArray(filters[key])) {
+      result = result.filter(item => filters[key].includes(item[key]));
+      delete filters[key];
+    }
+  });
+
+  if (result.length === 0) result = object;
+
+  Object.keys(filters).forEach((key) => {
+    result = result.filter(item => item[key] === filters[key]);
+  });
+
+  return result;
 };
 
 module.exports = objectFilter;
--- test/filterObjec.test.js
+++ test/filterObjec.test.js
@@ -8,4 +8,28 @@ describe('the object filter', () => {
     const result = JSON.stringify(byArray(messages.data, messages.input.array));
     expect(result).to.equal(JSON.stringify(messages.output.array));
   });
+
+  it('filters an Array when given an queryOptions object', () => {
+    // example queryOptions { questionId: 84 }
+    const result = JSON.stringify(byObject(messages.data, messages.input.object));
+    expect(result).to.equal(JSON.stringify(messages.output.object));
+  });
+
+  it('filters an Array when given a queryOptions object with multiple different filters', () => {
+    // { age: 25-29, sex: M }
+    const result = JSON.stringify(byObject(users.data, { age: '25-29', sex: 'M' }));
+    expect(result).to.equal(JSON.stringify(users.multipleDifferentFilters.output));
+  });
+
+  it('filters an Array when given a queryOptions object with multiple overlapping filters', () => {
+    // example queryOptions { age: [ '25-29', '50-59' ] }
+    const result = JSON.stringify(byObject(users.data, users.input.object));
+    expect(result).to.equal(JSON.stringify(users.output.object));
+  });
+
+  it('filters an Array when given a queryOptions object with multiple overlapping and different filters', () => {
+    // { age: '25-29', age: '50-59', income: '40,000-50,000', income: '<20,000', sex: 'F' }
+    const result = JSON.stringify(byObject(users.data, users.multiOverlappingDiff.input));
+    expect(result).to.equal(JSON.stringify(users.multiOverlappingDiff.output));
+  });
 });
